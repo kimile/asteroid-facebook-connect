5f91c015667ce5ecf56f4015e14ea0da55e5f278
strikedaemon <strikedaemon@gmail.com>
2016-03-15 22:51:50 +0000

    Added only the secure user information and resolve the client promise with the user object

--- client.js
+++ client.js
@@ -9,32 +9,36 @@
  *    http://plugins.cordova.io/#/package/com.phonegap.plugins.facebookconnect
  * Ensure that you've installed the meteor package so that the Asteroid.loginWithFacebook call is answered
  */
-Asteroid.prototype.loginWithFacebook = function(permissions) {
-  var self = this;
-  var deferred = Q.defer();
-  var done = function(err, res) {
-    if (err) {
-      delete self.userId;
-      delete self.loggedIn;
-      Asteroid.utils.multiStorage.del(self._host + "__" + self._instanceId + "__login_token__");
-      deferred.reject(err);
-      self._emit("loginError", err);
-    } else {
-      self.userId = res.id;
-      self.loggedIn = true;
-      Asteroid.utils.multiStorage.set(self._host + "__" + self._instanceId + "__login_token__", res.token);
-      self._emit("login", res.id);
-      deferred.resolve(res.id);
-    }
-  }
-  facebookConnectPlugin.login(permissions || [ "public_profile", "email" ], function(data) {
-    self.call('Asteroid.loginWithFacebook', data).result.then(function(res) {
-      done(null, res);
-    }, function(err) {
-      done(new Error(error.reason))
+Asteroid.prototype.loginWithFacebook = function (permissions) {
+
+    var self = this;
+    var deferred = Q.defer();
+
+    var done = function (err, res) {
+        if (err) {
+            delete self.userId;
+            delete self.loggedIn;
+            Asteroid.utils.multiStorage.del(self._host + "__" + self._instanceId + "__login_token__");
+            deferred.reject(err);
+            self._emit("loginError", err);
+        } else {
+            self.userId = res.id;
+            self.loggedIn = true;
+            Asteroid.utils.multiStorage.set(self._host + "__" + self._instanceId + "__login_token__", res.token);
+            self._emit("login", res.user);
+            deferred.resolve(res.user);
+        }
+    };
+
+    facebookConnectPlugin.login(permissions || ["public_profile", "email"], function (data) {
+        self.call('Asteroid.loginWithFacebook', data).result.then(function (res) {
+            done(null, res);
+        }, function (err) {
+            done(new Error(error.reason))
+        });
+    }, function (err) {
+        done(new Error(err.errorMessage));
     });
-  }, function(err) {
-    done(new Error(err.errorMessage));
-  });
-  return deferred.promise;
-}
+
+    return deferred.promise;
+};
--- facebook.js
+++ facebook.js
@@ -1,108 +1,121 @@
 var getIdentity = function (accessToken) {
-  try {
-    return Meteor.http.get("https://graph.facebook.com/v2.2/me", {
-      params: {access_token: accessToken}}).data;
-  } catch (err) {
-    throw _.extend(new Error("Failed to fetch identity from Facebook. " + err.message),
-                   {response: err.response});
-  }
+    try {
+        return Meteor.http.get("https://graph.facebook.com/v2.2/me", {
+            params: {access_token: accessToken}
+        }).data;
+    } catch (err) {
+        throw _.extend(new Error("Failed to fetch identity from Facebook. " + err.message),
+            {response: err.response});
+    }
 };
 
 var getProfilePicture = function (accessToken) {
-  try {
-    return Meteor.http.get("https://graph.facebook.com/v2.0/me/picture/?redirect=false", {
-      params: {access_token: accessToken}}).data.data.url;
-  } catch (err) {
-    throw _.extend(new Error("Failed to fetch identity from Facebook. " + err.message),
-                   {response: err.response});
-  }
+    try {
+        return Meteor.http.get("https://graph.facebook.com/v2.0/me/picture/?redirect=false", {
+            params: {access_token: accessToken}
+        }).data.data.url;
+    } catch (err) {
+        throw _.extend(new Error("Failed to fetch identity from Facebook. " + err.message),
+            {response: err.response});
+    }
 };
 
-var getProfileFields = function() {
-  return [
-    "name",
-    "location",
-    "email",
-    "first_name",
-    "last_name",
-    "locale",
-    "age_range"
-  ]
+var getProfileFields = function () {
+    return [
+        "name",
+        "location",
+        "email",
+        "first_name",
+        "last_name",
+        "locale",
+        "age_range"
+    ]
 }
 
 Meteor.methods({
-  'Asteroid.loginWithFacebook': function(data) {
-    if (data.status !== "connected") {
-      throw Meteor.Error(500, "Unable to login or register with facebook", "Facebook status is not connected");
-    }
-
-    var loginRequest = data.authResponse;
-
-    var identity = getIdentity(loginRequest.accessToken);
-    var profilePicture = getProfilePicture(loginRequest.accessToken);
-
-    var serviceData = {
-        accessToken: loginRequest.accessToken,
-        expiresAt: (+new Date) + (1000 * loginRequest.expiresIn)
-    };
-
-    var whitelisted = ['id', 'email', 'name', 'first_name',
-        'last_name', 'link', 'username', 'gender', 'locale', 'age_range'];
-
-    var fields = _.pick(identity, whitelisted);
-    _.extend(serviceData, fields);
-
-    var options = {profile: {}};
-    var profileFields = _.pick(identity, getProfileFields());
-    _.extend(options.profile, profileFields);
-
-    options.profile.avatar = profilePicture;
-
-    var ref = null;
-
-    try {
-      // https://github.com/meteor/meteor/blob/devel/packages/accounts-base/accounts_server.js#L1129
-      ref = Accounts.updateOrCreateUserFromExternalService("facebook", serviceData, options);
-    } catch (e) {
-      if (e.reason === "Email already exists.") {
-        // this user already signed up a local account, lets merge the facebook service and finish logging in
-        // this email address might already exist, in which case our Accounts.updateOrCreateUserFromExternalService will fail
-        // so we have to handle this case manually and try our best to merge identities.
-        var existingUser = Meteor.users.findOne({ 'emails.address': identity.email });
-        if ( existingUser ) {
-          if ( identity.verified ) {
-            Meteor.users.update({ _id: existingUser._id }, { $set: { 'services.facebook': serviceData }});
-            ref = { userId: existingUser._id };
-            console.log('Merged facebook identity with existing local user '+existingUser._id);
-          } else {
-            throw Meteor.Error(403, "Refusing to merge unverified facebook identity with existing user")
-          }
+    'Asteroid.loginWithFacebook': function (data) {
+        if (data.status !== "connected") {
+            throw Meteor.Error(500, "Unable to login or register with facebook", "Facebook status is not connected");
         }
-      } else {
-        throw Meteor.Error(e.error, e.reason);
-      }
+
+        var loginRequest = data.authResponse;
+
+        var identity = getIdentity(loginRequest.accessToken);
+        var profilePicture = getProfilePicture(loginRequest.accessToken);
+
+        var serviceData = {
+            accessToken: loginRequest.accessToken,
+            expiresAt: (+new Date) + (1000 * loginRequest.expiresIn)
+        };
+
+        var whitelisted = ['id', 'email', 'name', 'first_name',
+            'last_name', 'link', 'username', 'gender', 'locale', 'age_range'];
+
+        var fields = _.pick(identity, whitelisted);
+        _.extend(serviceData, fields);
+
+        var options = {profile: {}};
+        var profileFields = _.pick(identity, getProfileFields());
+        _.extend(options.profile, profileFields);
+
+        options.profile.avatar = profilePicture;
+
+        var ref = null;
+
+        try {
+            // https://github.com/meteor/meteor/blob/devel/packages/accounts-base/accounts_server.js#L1129
+            ref = Accounts.updateOrCreateUserFromExternalService("facebook", serviceData, options);
+        } catch (e) {
+            if (e.reason === "Email already exists.") {
+                // this user already signed up a local account, lets merge the facebook service and finish logging in
+                // this email address might already exist, in which case our Accounts.updateOrCreateUserFromExternalService will fail
+                // so we have to handle this case manually and try our best to merge identities.
+                var existingUser = Meteor.users.findOne({'emails.address': identity.email});
+                if (existingUser) {
+                    if (identity.verified) {
+                        Meteor.users.update({_id: existingUser._id}, {$set: {'services.facebook': serviceData}});
+                        ref = {userId: existingUser._id};
+                        console.log('Merged facebook identity with existing local user ' + existingUser._id);
+                    } else {
+                        throw Meteor.Error(403, "Refusing to merge unverified facebook identity with existing user")
+                    }
+                }
+            } else {
+                throw Meteor.Error(e.error, e.reason);
+            }
+        }
+
+        //creating the token and adding to the user
+        var stampedToken = Accounts._generateStampedLoginToken();
+        //hashing is something added with Meteor 0.7.x,
+        //you don't need to do hashing in previous versions
+        var hashStampedToken = Accounts._hashStampedToken(stampedToken);
+
+        Meteor.users.update(ref.userId, {
+            $push: {'services.resume.loginTokens': hashStampedToken}
+        });
+
+        // set the user identifier for this connection
+        this.setUserId(ref.userId);
+
+        var user = Meteor.user();
+
+        //sending token along with the userId
+        return {
+            id: ref.userId,
+            token: stampedToken.token,
+
+            // include the user details
+            user: {
+                _id: user._id,
+                profile: user.profile,
+                roles: user.roles,
+                services: {
+                    facebook: {
+                        id: user.services.facebook.id
+                    }
+                }
+            }
+        };
     }
-
-    //creating the token and adding to the user
-    var stampedToken = Accounts._generateStampedLoginToken();
-    //hashing is something added with Meteor 0.7.x, 
-    //you don't need to do hashing in previous versions
-    var hashStampedToken = Accounts._hashStampedToken(stampedToken);
-
-    Meteor.users.update(ref.userId, {
-      $push: {'services.resume.loginTokens': hashStampedToken}
-    });
-    
-    // set the user identifier for this connection
-    this.setUserId(ref.userId);
-
-    //sending token along with the userId
-    return {
-      id: ref.userId,
-      token: stampedToken.token,
-      
-      // include the user details
-      user: Meteor.user()
-    }
-  }
 });
